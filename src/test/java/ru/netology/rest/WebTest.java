013a6c8d4ff6b5f5384025ae498f4c568c278795
Ulugbek2080 <ulugbekabduzamilov@gmail.com>
2026-09-03 14:31:31 +0500

    У валидного входа правильный ожидаемый текст

--- src/test/java/ru/netology/rest/WebTest.java
+++ src/test/java/ru/netology/rest/WebTest.java
@@ -7,91 +7,77 @@ import org.junit.jupiter.api.Test;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$$;
 import static com.codeborne.selenide.Selenide.open;
+
 public class WebTest {
 
     @BeforeEach
     void openPage() {
         open("http://localhost:9999");
     }
+
     private String date(int plusDays) {
         return LocalDate.now().plusDays(plusDays).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
-    private void submit(String city, String date, String name,
-                        String phone, boolean agree) {
 
-        $("[data-test-id=city] input")
-                .setValue(city);
-
-        $("[data-test-id=date] input")
-                .setValue(date);
-
-        $("[data-test-id=name] input")
-                .setValue(name);
-
-        $("[data-test-id=phone] input")
-                .setValue(phone);
-
+    private void submit(String city, String date, String name, String phone, boolean agree) {
+        $("[data-test-id=city] input").setValue(city);
+        $("[data-test-id=date] input").setValue(date);
+        $("[data-test-id=name] input").setValue(name);
+        $("[data-test-id=phone] input").setValue(phone);
         if (agree) {
-            $("[data-test-id=agreement]")
-                    .click();
+            $("[data-test-id=agreement]").click();
         }
-
-        $$("button")
-                .findBy(text("Запланировать"))
-                .click();
+        $$("button").findBy(text("Запланировать")).click();
     }
 
     private SelenideElement error(String field) {
-        return $("[data-test-id=" + field + "] .input__sub");
+        return $("[data-test-id=" + field + "].input_invalid .input__sub");
+    }
 
+    @Test
+    void shouldSubmitValidForm() {
+        String meetingDate = date(3);
+        submit("Москва", meetingDate, "Иван Петров", "+79012345678", true);
+        $("[data-test-id=success-notification]")
+                .shouldBe(visible)
+                .shouldHave(text("Встреча успешно запланирована на " + meetingDate));
     }
 
     @Test
     void shouldRejectInvalidName() {
         submit("Москва", date(3), "Ivan Petrov", "+79012345678", true);
-        $("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
-        error("name").shouldHave(
+        error("name").shouldBe(visible).shouldHave(
                 exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
     @Test
     void shouldRejectEmptyName() {
         submit("Москва", date(3), "", "+79012345678", true);
-        $("[data-test-id=name]").shouldHave(cssClass("input_invalid"));
-        error("name").shouldHave(exactText("Поле обязательно для заполнения"));
+        error("name").shouldBe(visible).shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldRejectInvalidPhone() {
         submit("Москва", date(3), "Иван Петров", "+790127", true);
-        $("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
-        error("phone").shouldHave(
-                text("Телефон указан неверно. Должно быть 11 цифр"));
+        error("phone").shouldBe(visible).shouldHave(
+                exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
 
     @Test
     void shouldRejectEmptyPhone() {
         submit("Москва", date(3), "Иван Петров", "", true);
-        $("[data-test-id=phone]").shouldHave(cssClass("input_invalid"));
-        error("phone").shouldHave(exactText("Поле обязательно для заполнения"));
+        error("phone").shouldBe(visible).shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
     void shouldRejectUncheckedAgreement() {
         submit("Москва", date(3), "Иван Петров", "+79012345678", false);
-        $("[data-test-id=agreement]").shouldHave(cssClass("input_invalid"));
-        $("[data-test-id=order-success]").shouldNotBe(visible);
-    }
-
-    @Test
-    void shouldSubmitValidForm() {
-        submit("Москва", date(3), "Иван Петров", "+79012345699", true);
-        $("[data-test-id=order-success]")
-                .shouldBe(visible)
-                .shouldHave(text("Встреча успешно запланирована на " + date(3)));
+        $("[data-test-id=agreement].input_invalid").shouldBe(visible);
     }
 }
